935fa9eea8e9e7dd00f54b7814f7ffc4fd88d4c8
Irina <149883699+irinakravtsova@users.noreply.github.com>
2024-06-24 13:22:10 +0300

    Delete src directory

--- src/src/pages/Counter/selectors.js
+++
@@ -1,7 +0,0 @@
-export const getCounter = state => state.counter.value;
-export const getIsLoading = state => state.counter.isLoading;
-export const getIsError = state => state.counter.isError;
-
-export const getIsSaveLoading = state => state.counter.isSaveLoading;
-export const getIsSaveSucces = state => state.counter.isSaveSucces;
-export const getIsSaveError = state => state.counter.isSaveError;
